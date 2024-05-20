913809ebe78e2f28685a1366970f7ab811042ddf
MRxwr <nasserhatab@gmail.com>
2024-05-20 06:27:14 +0300

    save

--- pos/checkout.php
+++
@@ -1,6 +0,0 @@
-<?php session_start() ?>
-<?php include 'templates/header.php'; ?>
-<?php include '../admin/includes/config.php'; ?>
-<div style="padding-top:20px"></div>
-<?php include 'templates/checkout.php'; ?>
-<?php include 'templates/footer.php'; ?>
--- pos/help.php
+++
@@ -1,4 +0,0 @@
-<?php include 'templates/header.php'; ?>
-<div style="padding-top:40px"></div>
-<?php include 'templates/help.php'; ?>
-<?php include 'templates/footer.php'; ?>
