52492cb76e610b0ccfb160ab635b31b2deab377a
MRxwr <nasserhatab@gmail.com>
2024-05-20 04:32:15 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "store";
+$username = "u409066344_salahSysUSER";
+$password = "N@b$90949089";
+$dbname = "u409066344_salahSysDB";
 $dbconnect = new MySQLi($servername,$username,$password,$dbname);
 if ( $dbconnect->connect_error ){
 die("Connection Failed: " .$dbconnect->connect_error );
