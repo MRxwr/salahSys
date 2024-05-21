980e491425edc3aa85cc809f7209ad0f3a89f333
MRxwr <nasserhatab@gmail.com>
2024-05-21 03:10:48 +0300

    Update bladeAreas.php

--- admin/views/bladeAreas.php
+++ admin/views/bladeAreas.php
@@ -5,12 +5,6 @@ if( isset($_GET["delId"]) && !empty($_GET["delId"]) ){
 	}
 }
 
-if( isset($_POST["setDefaultPrice"]) && !empty($_POST["setDefaultPrice"]) ){
-	if( updateDB('areas',array('charges'=> $_POST["setDefaultPrice"]),"`id` != '0'") ){
-		header("LOCATION: ?v=Areas");
-	}
-}
-
 if( isset($_POST["enTitle"]) ){
 	$id = $_POST["update"];
 	unset($_POST["update"]);
@@ -60,12 +54,6 @@ if( isset($_POST["enTitle"]) ){
 			<label><?php echo direction("Arabic Title","العنوان بالعربي") ?></label>
 			<input type="text" name="arTitle" class="form-control" required>
 			</div>
-			<?php /*
-			<div class="col-md-6">
-			<label><?php echo direction("Charge","السعر") ?></label>
-			<input type="float" name="charges" class="form-control" required>
-			</div>
-			*/ ?>
 			
 			<div class="col-md-6" style="margin-top:10px">
 			<input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
@@ -76,36 +64,7 @@ if( isset($_POST["enTitle"]) ){
 </div>
 </div>
 </div>
-</div>
-
-<?php /*
-<div class="col-sm-12">
-<div class="panel panel-default card-view">
-<div class="panel-heading">
-<div class="pull-left">
-	<h6 class="panel-title txt-dark"><?php echo direction("Set default price","سعر توصيل موحد") ?></h6>
-</div>
-	<div class="clearfix"></div>
-</div>
-<div class="panel-wrapper collapse in">
-<div class="panel-body">
-	<form class="" method="POST" action="" enctype="multipart/form-data">
-		<div class="row m-0">
-			<div class="col-md-6">
-			<label><?php echo direction("Default Price","السعر الموحد") ?></label>
-			<input type="float" name="setDefaultPrice" class="form-control" required>
-			</div>
-			
-			<div class="col-md-6" style="margin-top:10px">
-			<input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
-			</div>
-		</div>
-	</form>
-</div>
-</div>
-</div>
-</div>
-*/ ?>		
+</div>		
 				<!-- Bordered Table -->
 <div class="col-sm-12">
 <div class="panel panel-default card-view">
@@ -124,7 +83,6 @@ if( isset($_POST["enTitle"]) ){
 		<tr>
 		<th><?php echo direction("English Title","العنوان بالإنجليزي") ?></th>
 		<th><?php echo direction("Arabic Title","العنوان بالعربي") ?></th>
-		<?php /*<th><?php echo direction("Charge","السعر") ?></th> */ ?>
 		<th class="text-nowrap"><?php echo direction("الخيارات","Actions") ?></th>
 		</tr>
 		</thead>
@@ -139,7 +97,6 @@ if( isset($_POST["enTitle"]) ){
 				<tr>
 				<td id="enTitle<?php echo $areas[$i]["id"]?>" ><?php echo $areas[$i]["enTitle"] ?></td>
 				<td id="arTitle<?php echo $areas[$i]["id"]?>" ><?php echo $areas[$i]["arTitle"] ?></td>
-				<?php /*<td id="charges<?php echo $areas[$i]["id"]?>" ><?php echo $areas[$i]["charges"] ?></td> */ ?>
 				<td class="text-nowrap">
 					<a id="<?php echo $areas[$i]["id"] ?>" class="mr-25 edit" data-toggle="tooltip" data-original-title="Edit"> <i class="fa fa-pencil text-inverse m-r-10"></i>
 					</a>
@@ -167,9 +124,7 @@ if( isset($_POST["enTitle"]) ){
 		var id = $(this).attr("id");
 		var enTitle = $("#enTitle"+id).html();
 		var arTitle = $("#arTitle"+id).html();
-		//var charges = $("#charges"+id).html();
 		$("input[name=enTitle]").val(enTitle);
-		//$("input[name=charges]").val(charges);
 		$("input[name=update]").val(id);
 		$("input[name=arTitle]").val(arTitle);
 		$("input[name=enTitle]").focus()
