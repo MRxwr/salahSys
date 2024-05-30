bc5fe33ee100c78db22504507efc2cf5d3cb285e
MRxwr <nasserhatab@gmail.com>
2024-05-31 00:21:33 +0300

    Create check.php

---
+++ api/check.php
@@ -0,0 +1,16 @@
+<?php 
+require_once("../admin/includes/config.php");
+require_once("../admin/includes/functions.php");
+if( isset($_GET["date"]) && !empty($_GET["date"]) ){
+    $date = $_GET["date"];
+    if( $checkDate = selectDBNew("applications",[$date],"`testDate` LIKE ?","") ){
+        if ( sizeof($checkDate) > 1 ){
+            echo "false";
+        }else{
+            echo "true";
+        }
+    }else{
+        echo "true";
+    }
+}
+?>
